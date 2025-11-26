8c4d37c93f3a819abdef493702e5d02a7e1ebdd6
Aaron <junewhj@qq.com>
2025-11-26 20:45:29 +0800

    Add compatibility for recently updated data

--- mkdocs_recently_updated_docs/plugin.py
+++ mkdocs_recently_updated_docs/plugin.py
@@ -2,7 +2,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
 from mkdocs.plugins import BasePlugin
 from mkdocs.config import config_options
-from mkdocs_document_dates.utils import get_recently_updated_files
+from mkdocs_document_dates.utils import load_git_last_updated_date, get_recently_updated_files
 
 
 class RecentlyUpdatedPlugin(BasePlugin):
@@ -17,20 +17,19 @@ class RecentlyUpdatedPlugin(BasePlugin):
 
         self.recent_docs_html = None
 
-    def on_nav(self, nav, config, files):
+    def on_env(self, env, config, files):
         limit = self.config.get('limit')
         exclude_list = self.config.get('exclude')
         template_path = self.config.get('template')
 
         docs_dir = Path(config['docs_dir'])
-
-        # 获取 docs 目录下最近更新的文档
-        _, recently_modified_files = get_recently_updated_files(docs_dir, files, exclude_list, limit, True)
+        last_updated_dates = load_git_last_updated_date(docs_dir)
+        recently_modified_files = get_recently_updated_files(last_updated_dates, files, exclude_list, limit, True)
 
         # 渲染HTML
         self.recent_docs_html = self._render_recently_updated_html(docs_dir, template_path, recently_modified_files)
 
-        return nav
+        return env
 
     def _render_recently_updated_html(self, docs_dir, template_path, recently_updated_data):
         # 获取自定义模板路径
@@ -49,15 +48,15 @@ class RecentlyUpdatedPlugin(BasePlugin):
 
         # 加载模板
         env = Environment(
-            loader=FileSystemLoader(str(template_dir)),
-            autoescape=select_autoescape(["html", "xml"])
+            loader = FileSystemLoader(str(template_dir)),
+            autoescape = select_autoescape(["html", "xml"])
         )
         template = env.get_template(template_file)
 
         # 渲染模板
         return template.render(recent_docs=recently_updated_data)
 
-    def on_page_markdown(self, markdown, page, config, files):
-        if '\n<!-- RECENTLY_UPDATED_DOCS -->' in markdown:
-            markdown = markdown.replace('\n<!-- RECENTLY_UPDATED_DOCS -->', self.recent_docs_html or '')
-        return markdown
+    def on_post_page(self, output, page, config):
+        if '\n<!-- RECENTLY_UPDATED_DOCS -->' in output:
+            output = output.replace('\n<!-- RECENTLY_UPDATED_DOCS -->', self.recent_docs_html or '')
+        return output
